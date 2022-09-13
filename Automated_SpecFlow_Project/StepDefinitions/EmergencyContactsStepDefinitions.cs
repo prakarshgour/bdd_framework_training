b60c8b8b93bed894516c71ec5540becd6fce6477
Prakarsh Gour <Prakarsh.Gour@mindbodyonline.com>
2022-09-13 11:01:36 +0530

    Updated ThenIShopuldBeNavigatedToViewEmergencyContactsSection() in EmergencyContactStepDefinitions.cs

--- Automated_SpecFlow_Project/StepDefinitions/EmergencyContactsStepDefinitions.cs
+++ Automated_SpecFlow_Project/StepDefinitions/EmergencyContactsStepDefinitions.cs
@@ -8,11 +8,7 @@ namespace Automated_SpecFlow_Project.StepDefinitions
     [Binding]
     public class EmergencyContactsStepDefinitions
     {
-        public string name;
-        public string relationship;
-        public string homeTelephone;
-        public string mobile;
-        public string workTelephone;
+        Table dataTable;
 
         [When(@"I click on My Info")]
         public void WhenIClickOnMyInfo()
@@ -35,11 +31,13 @@ namespace Automated_SpecFlow_Project.StepDefinitions
         [When(@"I fill emergency contact details")]
         public void WhenIFillEmergencyContactDetails(Table table)
         {
-            name = table.Rows[0]["name"].ToString();
-            relationship = table.Rows[0]["relationship"].ToString();
-            homeTelephone = table.Rows[0]["home_telephone"].ToString();
-            mobile = table.Rows[0]["mobile"].ToString();
-            workTelephone = table.Rows[0]["work_telephone"].ToString();
+            dataTable = table;
+
+            string name = table.Rows[0]["name"].ToString();
+            string relationship = table.Rows[0]["relationship"].ToString();
+            string homeTelephone = table.Rows[0]["home_telephone"].ToString();
+            string mobile = table.Rows[0]["mobile"].ToString();
+            string workTelephone = table.Rows[0]["work_telephone"].ToString();
 
             // Name
             AutomationHooks.driver.FindElement(By.XPath("//label[text() = 'Name']/following::input")).SendKeys(name);
@@ -66,7 +64,18 @@ namespace Automated_SpecFlow_Project.StepDefinitions
         [Then(@"I shopuld be navigated to view emergency contacts section")]
         public void ThenIShopuldBeNavigatedToViewEmergencyContactsSection()
         {
-            
+            string actualData = AutomationHooks.driver.FindElement(By.XPath("//div[@class='oxd-table']")).Text;
+            string expectedName = dataTable.Rows[0]["name"];
+            string expectedRelationship = dataTable.Rows[0]["relationship"];
+            string expectedHomeTelephone = dataTable.Rows[0]["home_telephone"];
+            string expectedMobile = dataTable.Rows[0]["mobile"];
+            string expectedWorkTelephone = dataTable.Rows[0]["work_telephone"];
+
+            Assert.Contains(expectedName, actualData);
+            Assert.Contains(expectedRelationship, actualData);
+            Assert.Contains(expectedHomeTelephone, actualData);
+            Assert.Contains(expectedMobile, actualData);
+            Assert.Contains(expectedWorkTelephone, actualData);
         }
     }
 }
